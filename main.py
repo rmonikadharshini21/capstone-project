84744a7fe65b144e46c08553f8f99f4c0277326d
Monika Dharshini <monikadharshini3@gmail.com>
2026-09-21 15:58:44 +0530

    Fix frontend CORS deployment

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import auth, waste, admin, worker, ai
+
 
 app = FastAPI(
     title="Smart Waste Management API",
     version="1.0.0"
 )
 
+
 app.add_middleware(
     CORSMiddleware,
+
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://capstone-project-1-rt16.onrender.com"
     ],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
