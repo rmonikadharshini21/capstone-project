350479d6f7cc394f733433369a258f0d08c1bffe
Monika Dharshini <monikadharshini3@gmail.com>
2026-09-21 15:49:21 +0530

    Connect API service to Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: "https://capstone-project-ds0d.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
